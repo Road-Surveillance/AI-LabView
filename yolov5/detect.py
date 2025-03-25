88e2f8ebc8b79a69f8bdb823d74fdff723c0ef54
Kefabd <abdoukefsi@gmail.com>
2025-03-25 01:53:50 +0100

    Color detection

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -154,6 +154,7 @@ def run(weights=ROOT / 'yolov5s.pt',  # model.pt path(s)
             if len(det):
                 # Rescale boxes from img_size to im0 size
                 det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+                print("test", det[:, :4])
 
                 # Print results
                 for c in det[:, -1].unique():
